9a094163aa388ad0cfeed050735203504511b8f0
Vinit Pawar <vinitpawar8305@gmail.com>
2025-08-19 01:35:50 +0530

    add url

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://react-app-backend-7fwu.onrender.com';// development 
+const API_BASE_URL = 'https://react-app-backend-7fwu.onrender.com/api';// development 
 // const API_BASE_URL = 'http://localhost:5000/api';
 
 // Create axios instance
